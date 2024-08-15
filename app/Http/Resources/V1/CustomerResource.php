a60aef9ba4cfd5989d6266aab9e16be3c3d22239
Htet Phone Aung <senseihtetphone@gmail.com>
2024-08-15 12:38:55 +0630

    api dev x laravel

---
+++ app/Http/Resources/V1/CustomerResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class CustomerResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'type' => $this->type,
+            'email' => $this->email,
+            'address' => $this->address,
+            'state' => $this->state,
+            'city' => $this->city,
+            'postalCode' => $this->postal_code
+        ];
+    }
+}
